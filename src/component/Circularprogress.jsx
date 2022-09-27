cbc1d637684398d77e063843b87a5e4eda93bb51
sudhirtawaniya <sudhirtawaniya30@gmail.com>
2022-09-27 10:17:06 +0530

    initial commit

---
+++ src/component/Circularprogress.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useEffect } from 'react';
+import "./cprogress.css"
+export function Circulaprogress(){
+ 
+return(
+    <>
+    <div className='container'>
+        <div className="outer">
+            <div className="inner">
+               
+            </div>
+
+
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+         <defs>
+            <linearGradient id="GradientColor">
+               <stop offset="0%" stop-color="#e91e63" />
+               <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+         </defs>
+         <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+ </svg>
+        </div>
+    </div>
+    </>
+);
+}
